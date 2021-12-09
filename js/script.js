56e74287f5bf15c1baf58eed45a01a85e798e1bb
muhammad-yazdian <muhammad.yazdian@gmail.com>
2021-12-09 14:25:38 +0100

    Added radion buttons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
-document.getElementById("btn-fix").onclick = function(){
-  document.getElementById("text-out").innerHTML = 
-    toTrueTitleCase(document.getElementById("text-in").value);
+const inputText = document.getElementById('text-in');
+const outputText = document.getElementById('text-out');
+
+function textFormat(){
+  return document.querySelector(
+    'input[name="select-text-format"]:checked').value;
+}
+
+const inputTextHandler = function(x){
+  updateText(x.target.value);
+}
+
+function radioButtonHandler(){
+  updateText(inputText.value);
+}
+
+inputText.addEventListener('input', inputTextHandler);
+
+function updateText(text){
+  switch(textFormat()){
+    case 'all-caps':
+      outputText.value = toTitleCase(text);
+      break;
+    case 'all-small':
+      outputText.value = text.toLowerCase();
+      break;
+    case 'true-title':
+      outputText.value = toTrueTitleCase(text);
+      break;
+  }
 }
